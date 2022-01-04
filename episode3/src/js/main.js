8158a2e3aa987d75b398f6a95e93e4b62393a49a
movie42 <44064122+movie42@users.noreply.github.com>
2022-01-05 07:17:38 +0900

    Trigonometry

--- episode3/src/js/main.js
+++ episode3/src/js/main.js
@@ -6,48 +6,70 @@ function drawCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
+  // 좌표 평면 중심 이동
   let centerY = height * 0.5;
   let centerX = width * 0.5;
+
+  // 중심에서 얼마나 이동할지
   let offset = height * 0.4;
-  let speed = 0.1;
+
+  // 속도
+  let speed = 0.05;
+
+  // 각
   let angle = 0;
 
-  function Animation(centerX, centerY, offset, speed, angle) {
-    function movingUpAndDown() {
-      let y = centerY + Math.sin(angle) * offset;
+  render();
 
-      ctx.clearRect(0, 0, width, height);
-      ctx.beginPath();
-      ctx.arc(centerX, y, 50, 0, Math.PI * 2, false);
-      ctx.fill();
+  function render() {
+    let y = centerY + Math.sin(angle) * offset;
 
-      angle += speed;
+    ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.arc(centerX, y, 50, 0, Math.PI * 2, false);
+    ctx.fill();
 
-      requestAnimationFrame(movingUpAndDown);
-    }
+    angle += speed;
 
-    function movingSideBySide() {
-      let x = centerX + Math.sin(angle) * offset;
-
-      ctx.clearRect(0, 0, width, height);
-      ctx.beginPath();
-      ctx.arc(x, centerY, 50, 0, Math.PI * 2, false);
-      ctx.fill();
-
-      angle += speed;
-
-      requestAnimationFrame(movingSideBySide);
-    }
-
-    return { movingUpAndDown, movingSideBySide };
+    requestAnimationFrame(render);
   }
 
-  const animation = new Animation(centerX, centerY, offset, speed, angle);
-  const animation2 = new Animation(centerX, centerY, offset, speed, angle);
+  // function Animation(centerX, centerY, offset, speed, angle) {
+  //   function movingUpAndDown() {
+  //     let y = centerY + Math.sin(angle) * offset;
 
-  animation.movingUpAndDown();
+  //     ctx.clearRect(0, 0, width, height);
+  //     ctx.beginPath();
+  //     ctx.arc(centerX, y, 50, 0, Math.PI * 2, false);
+  //     ctx.fill();
 
-  animation2.movingSideBySide();
+  //     angle += speed;
+
+  //     requestAnimationFrame(movingUpAndDown);
+  //   }
+
+  //   function movingSideBySide() {
+  //     let x = centerX + Math.sin(angle) * offset;
+
+  //     ctx.clearRect(0, 0, width, height);
+  //     ctx.beginPath();
+  //     ctx.arc(x, centerY, 50, 0, Math.PI * 2, false);
+  //     ctx.fill();
+
+  //     angle += speed;
+
+  //     requestAnimationFrame(movingSideBySide);
+  //   }
+
+  //   return { movingUpAndDown, movingSideBySide };
+  // }
+
+  // const animation = new Animation(centerX, centerY, offset, speed, angle);
+  // const animation2 = new Animation(centerX, centerY, offset, speed, angle);
+
+  // animation.movingUpAndDown();
+
+  // animation2.movingSideBySide();
 }
 
 window.addEventListener("load", drawCanvas);
