1323486165095cc18edccb8679fe922913ca2e53
ko Hyunsu <movie3342@gmail.com>
2022-01-07 15:54:49 +0900

    change file

--- episode10/src/js/main.js
+++ episode10/src/js/main.js
@@ -6,46 +6,100 @@ function draw() {
   const ctx = canvas.getContext("2d");
   const width = window.innerWidth;
   const height = window.innerHeight;
+  const ship = particle.create(width / 2, height / 2, 0, 0);
+  const thrust = vector.create(0, 0);
+
+  let angle = 0;
+  let turningLeft = false;
+  let turningRight = false;
+  let thrusting = false;
 
   canvas.width = width;
   canvas.height = height;
 
-  const sun = particle.create(width / 2, height / 2, 0, 0);
-  const planet = particle.create(width / 2 + 200, height / 2, 10, -Math.PI / 2);
-
-  sun.mass = 20000;
-
   update();
+
+  document.body.addEventListener("keydown", function (event) {
+    switch (event.key) {
+      case "ArrowUp":
+        thrusting = true;
+        break;
+      case "ArrowLeft":
+        turningLeft = true;
+        break;
+      case "ArrowRight":
+        turningRight = true;
+        break;
+      default:
+        break;
+    }
+  });
+
+  document.body.addEventListener("keyup", function (event) {
+    switch (event.key) {
+      case "ArrowUp":
+        thrusting = false;
+        break;
+      case "ArrowLeft":
+        turningLeft = false;
+        break;
+      case "ArrowRight":
+        turningRight = false;
+        break;
+      default:
+        break;
+    }
+  });
 
   function update() {
     ctx.clearRect(0, 0, width, height);
 
-    planet.gravitateTo(sun);
-    planet.update();
+    if (turningLeft) {
+      angle -= 0.05;
+    }
+
+    if (turningRight) {
+      angle += 0.05;
+    }
+
+    thrust.setAngle(angle);
+    if (thrusting) {
+      thrust.setLength(0.1);
+    } else {
+      thrust.setLength(0);
+    }
+
+    ship.accelerate(thrust);
+    ship.update();
+    ctx.save();
+    ctx.translate(ship.position.getX(), ship.position.getY());
+    ctx.rotate(angle);
 
     ctx.beginPath();
-    ctx.fillStyle = "#ffff00";
-    ctx.arc(
-      sun.position.getX(),
-      sun.position.getY(),
-      20,
-      0,
-      Math.PI * 2,
-      false,
-    );
-    ctx.fill();
+    ctx.moveTo(10, 0);
+    ctx.lineTo(-10, -7);
+    ctx.lineTo(-10, 7);
+    ctx.lineTo(10, 0);
+    if (thrusting) {
+      ctx.moveTo(-10, 0);
+      ctx.lineTo(-18, 0);
+    }
+    ctx.stroke();
 
-    ctx.beginPath();
-    ctx.fillStyle = "#0000ff";
-    ctx.arc(
-      planet.position.getX(),
-      planet.position.getY(),
-      5,
-      0,
-      Math.PI * 2,
-      false,
-    );
-    ctx.fill();
+    ctx.restore();
+
+    if (ship.position.getX() > width) {
+      ship.position.setX(0);
+    }
+    if (ship.position.getY() > height) {
+      ship.position.setY(0);
+    }
+    if (ship.position.getX() < 0) {
+      ship.position.setX(width);
+    }
+    if (ship.position.getY() < 0) {
+      ship.position.setY(height);
+    }
 
     requestAnimationFrame(update);
   }
--- episode10/src/js/particle.js
+++ episode10/src/js/particle.js
@@ -4,7 +4,6 @@ const particle = {
   positoin: null,
   velocity: null,
   gravity: null,
-  mass: 1,
 
   create: function (x, y, speed, direction, grav) {
     const obj = Object.create(this);
@@ -23,30 +22,6 @@ const particle = {
   update: function () {
     this.velocity.addTo(this.gravity);
     this.position.addTo(this.velocity);
-  },
-
-  angleTo: function (p2) {
-    return Math.atan2(
-      p2.position.getY() - this.position.getY(),
-      p2.position.getX() - this.position.getX(),
-    );
-  },
-
-  distanceTo: function (p2) {
-    let dx = p2.position.getX() - this.position.getX();
-    let dy = p2.position.getY() - this.position.getY();
-
-    return Math.sqrt(dx * dx + dy * dy);
-  },
-
-  gravitateTo: function (p2) {
-    let grav = vector.create(0, 0);
-    let dist = this.distanceTo(p2);
-
-    grav.setLength(p2.mass / (dist * dist));
-    grav.setAngle(this.angleTo(p2));
-
-    this.velocity.addTo(grav);
   },
 };
 
